d1e51644383888401c1156c5ae2452141dc14083
Glitch (mulberry-entrance) <none>
2018-01-19 02:10:28 +0000

    🎹🌔 Checkpoint server.js:311187/315 package.json:311187/11 views/index.html:311187/0

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var express = require('express')
 var helmet = require('helmet')
 var cors = require('cors')
 var bodyParser = require('body-parser')
+var parallel = require('run-parallel')
 var request = require('request-promise-native')
+var parser = require('rss-parser')
 
 const PORT = 5823
 
@@ -15,48 +17,47 @@ var app = express()
 app.use(helmet())
 app.use(cors())
 app.use(bodyParser.json({ limit: '50mb' }))
-app.post('/stream', async function (req, res) => {
+app.post('/stream', async function (req, res) {
   var sources = req.body.sources
-        
-  res.send()
-  
          
+  fetchFeeds(sources, function (feeds) {
+      res.send(feeds)
+  })
 })
 
 http.createServer(app).listen(PORT)
 
 console.log('Listening on port', PORT)
 
-function fetchFeeds () {
-  var jobs = state.sources.map(fetchingJob)
+function fetchFeeds (sources, callback) {
+  var jobs = sources.map(fetchingJob)
 
-  parallel(jobs, function () {
-    state.entries.sort(sortByDate)
-    state.fetching = false
+  parallel(jobs, function (feeds) {
+    var entries = entries.reduce(concat).sort(sortByDate)
 
-    emitter.emit('render')
+    callback(feeds)
   })
 }
 
-  function fetchingJob (source) {
-    return function (done) {
-      try {
-        var url = new URL(source)
-        parser.parseURL(url.href, function (err, parsed) {
-          if (err) return console.error(err)
-          parsed.feed.entries.forEach(entry => {
-            entry.date = new Date(entry.isoDate)
-            state.entries.push(entry)
-          })
-
-          done()
+function fetchingJob (source) {
+  var entries = []
+  return function (done) {
+    try {
+      var url = new URL(source)
+      parser.parseURL(url.href, function (err, parsed) {
+        if (err) return console.error(err)
+        parsed.feed.entries.forEach(entry => {
+          entry.date = new Date(entry.isoDate)
+          entries.push(entry)
         })
-      } catch (err) {
-        console.info(err)
-      }
+
+        done(entries)
+      })
+    } catch (err) {
+      console.info(err)
     }
   }
-})
+}
 
 function sortByDate (a, b) {
   if (a.date > b.date) return -1
